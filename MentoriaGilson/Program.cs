48df386b98a8938ceb9ef43a6526ca1d6c258fa6
Guiando <Guiando@G036>
2022-05-24 17:53:50 -0300

    Add project files.

---
+++ MentoriaGilson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MentoriaGilson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meuNome = "Gilson"; // declarei uma string -> "PRONUNCIA": meu nome recebe Gilson
+           string meuSegundoNome = "Ravaiani"; // outra maneira de declarar uma variável
+           string meuNomeCompleto = meuNome + " " + meuSegundoNome; //concatenação
+           var mensagemBoasVindas = ($"Olá, meu nome é: {meuNome}"); //Outro modo de concatenar
+
+
+            //"" -> varios caracteres
+            //'' -> Apenas um caracter
+                       
+            Console.WriteLine(meuNomeCompleto);
+            Console.WriteLine(meuNome + meuSegundoNome);
+            Console.WriteLine(mensagemBoasVindas);
+        }
+    }
+}
